9a84e846363f7b1ade5fa2b6a4a80a75285550b3
Ahmed Abd El-Aziz Ezzat Mohamed <aaezzat@mcit.gov.eg>
2023-10-02 11:48:48 +0300

    Add packbak model and db table with one to one relation with super hero

---
+++ SuperHeroAPI/Models/Backpack.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperHeroAPI.Models
+{
+    public class Backpack
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int SuperHeroId { get; set; }
+        public SuperHero SuperHero { get; set; }
+    }
+}
